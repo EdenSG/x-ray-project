fa186f656da0dd4cca846cbe6a7d1461eef3f7c5
Eden S.-G <edgro2693@gmail.com>
2015-01-02 11:53:56 -0500

    style/interaction work

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,22 @@
 var s = skrollr.init({
-	easing: {
+    easing: {
         //This easing will sure drive you crazy
         ten: function(p) {
             return Math.pow(p, 10);
         },
+        six: function(p) {
+            return Math.pow(p, 6);
+        },
         sixinv: function(p) {
-            return Math.pow(p, 1/6);
+            return Math.pow(p, 1 / 6);
+        },
+        teninv: function(p) {
+            return Math.pow(p, .1);
         },
     },
     smoothScrolling: false
 });
-function scrollTo (distance_percent) {
-	document.body.scrollTop = distance_percent / 100 * window.innerHeight;
+function scrollTo(distance_percent) {
+    document.body.scrollTop = distance_percent / 100 * window.innerHeight;
+    console.log('done');
 }
